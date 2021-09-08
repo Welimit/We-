550d4b7b65ce2b2dcc06ace43801df142bb5943a
Welimit <88602628+Welimit@users.noreply.github.com>
2021-09-08 10:29:29 +0530

    Add files via upload

---
+++ javascript project/JS open & close/index.js
@@ -0,0 +1,19 @@
+    const closedFace = document.querySelector('.close')
+    const openFace = document.querySelector('.open')
+
+    //Add Event Listener
+
+    closedFace.addEventListener('click', () => {
+        if(openFace.classList.contains('open')) {
+            openFace.classList.add('active');
+            closedFace.classList.remove('active');
+        }
+    });
+
+    openFace.addEventListener('click', () => {
+        if(closedFace.classList.contains('close')) {
+            closedFace.classList.add('active');
+            openFace.classList.remove('active');
+        }
+    }); 
+
